ed624646eba7a568d13e256e27c140b15bd59f8d
davidprager <david_prager@yahoo.com>
2023-09-07 18:52:53 -0400

    Initial commit

---
+++ Program1.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #6, Program 1
+Your name here
+"""
+
+# This program demonstrates the use of some functions
---
+++ Program2.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #6, Program 2
+Your name here
+"""
+
+# This program analyzes numbers based upon user input
